f6cafb85a94df6167504fc6ac0f20f1c2e43eba7
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-06-09 22:40:26 +0530

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from "astro-icon";
+import partytown from '@astrojs/partytown'
+
 
 
 import sitemap from '@astrojs/sitemap';
@@ -8,5 +10,9 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), icon()],
+	integrations: [mdx(), sitemap(), icon(), partytown({
+		config: {
+		  forward: ["dataLayer.push"],
+		},
+	}),],
 });
