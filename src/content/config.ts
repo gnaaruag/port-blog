7aa780da5c9cec5c32c64c43d021428abbfa8c37
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-06-10 23:06:00 +0530

    build error

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,23 @@
 import { defineCollection, z } from 'astro:content';
 
+// Blog collection definition
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+// Index collection definition
+const index = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
+export const collections = { blog, index };
